1b6c936019d5f61b2b3e40848f4d5ee9d020443c
Samad Amir <136898842+SamadAmir@users.noreply.github.com>
2024-06-14 18:26:46 +0500

    Main.java

--- LeetCode/Arrays and hashing/ZeroMatrix/src/Main.java
+++ LeetCode/Arrays and hashing/ZeroMatrix/src/Main.java
@@ -33,7 +33,6 @@ class Main {
 
         main.setZeroes(matrix);
 
-        // Print the modified matrix
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
